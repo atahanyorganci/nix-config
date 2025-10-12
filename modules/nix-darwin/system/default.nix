2353b2bf62cba4cf8c284397c0bd0deccd3b2191
Atahan Yorgancı <atahan.yorganci@synnada.ai>
2025-10-13 00:12:54 +0300

    feat: add `system` module for `nix-darwin`

---
+++ modules/nix-darwin/system/default.nix
@@ -0,0 +1,41 @@
+{ lib, config, user, ... }:
+let
+  userAppDir = "/Users/${user.username}/Applications/Home Manager Apps";
+  systemAppDir = "/System/Applications";
+in
+{
+  config = {
+    system.defaults = {
+      NSGlobalDomain."com.apple.trackpad.enableSecondaryClick" = true;
+      dock = {
+        mru-spaces = false;
+        persistent-apps = [
+          { app = "${userAppDir}/Firefox.app"; }
+          { app = "${userAppDir}/Visual Studio Code.app"; }
+          { app = "${userAppDir}/Ghostty.app"; }
+          { app = "${userAppDir}/Whatsapp.app"; }
+          { app = "${systemAppDir}/Mail.app"; }
+          { app = "${systemAppDir}/Notes.app"; }
+          { app = "${systemAppDir}/System Settings.app"; }
+        ];
+        show-recents = false;
+        tilesize = 48;
+        wvous-bl-corner = 1;
+        wvous-br-corner = 1;
+        wvous-tl-corner = 1;
+        wvous-tr-corner = 1;
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        ShowPathbar = true;
+        _FXShowPosixPathInTitle = true;
+        _FXSortFoldersFirst = true;
+      };
+      trackpad = {
+        Clicking = true;
+        TrackpadRightClick = true;
+      };
+    };
+  };
+}
