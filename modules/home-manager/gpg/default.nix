be3e309f5c76f984ba518889f2c0569389ac699b
Atahan Yorgancı <atahan.yorganci@synnada.ai>
2025-10-27 10:47:47 +0300

    feat: enable shell integrations for `gpg-agent`

--- modules/home-manager/gpg/default.nix
+++ modules/home-manager/gpg/default.nix
@@ -11,6 +11,10 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
+    enableBashIntegration = config.shell.bash.enable;
+    enableFishIntegration = config.shell.fish.enable;
+    enableNushellIntegration = config.shell.fish.enable;
+    enableZshIntegration = config.shell.zsh.enable;
   };
   home.sessionVariables = {
     SSH_AUTH_SOCK = "${config.home.homeDirectory}/.gnupg/S.gpg-agent.ssh";
