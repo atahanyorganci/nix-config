9fe37ea3b4d3be0556d6080e563f88ff98691ea2
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-12-18 20:40:18 +0300

    fix: temporarily remove `atuin`

--- modules/home-manager/shell/default.nix
+++ modules/home-manager/shell/default.nix
@@ -151,22 +151,6 @@ in
       enableNushellIntegration = cfg.nushell.enable;
       enableZshIntegration = cfg.zsh.enable;
     };
-    # atuin - ✨ Magical shell history
-    # GitHub Repository: https://github.com/atuinsh/atuin
-    programs.atuin = {
-      enable = true;
-      enableBashIntegration = cfg.bash.enable;
-      enableFishIntegration = cfg.fish.enable;
-      enableNushellIntegration = cfg.nushell.enable;
-      enableZshIntegration = cfg.zsh.enable;
-      settings = {
-        sync_address = "http://localhost:8888";
-      };
-      flags = [
-        "--disable-up-arrow"
-        "--disable-ctrl-r"
-      ];
-    };
     programs.delta = {
       enable = true;
       enableGitIntegration = config.git.enable;
