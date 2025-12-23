02a6be5463ce98395de9e65658a0e8426acee44f
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-12-23 14:27:05 +0300

    feat: determine `sops.gnupg.home` based on host system

--- modules/shared/sops/default.nix
+++ modules/shared/sops/default.nix
@@ -1,10 +1,18 @@
-{ ... }: {
+{ pkgs
+, user
+, lib
+, ...
+}:
+let
+  isDarwin = lib.strings.hasSuffix "darwin" pkgs.stdenv.hostPlatform.system;
+  gpgHome = if isDarwin then "/Users/${user.username}/.gnupg" else "/home/${user.username}/.gnupg";
+in {
   sops.defaultSopsFile = ../../../secrets/dummy.enc.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.secrets = {
     dummy_secret = { };
   };
-  sops.gnupg.home = "/Users/atahan/.gnupg";
+  sops.gnupg.home = gpgHome;
   sops.gnupg.sshKeyPaths = [ ];
   sops.age.sshKeyPaths = [ ];
 }
