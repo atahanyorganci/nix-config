03741ea03ac8367d4d50fe3d1aa4261aad0661c9
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-01-15 21:29:00 +0300

    chore: change formatting for `orbstack.nix`

--- hosts/orb/orbstack.nix
+++ hosts/orb/orbstack.nix
@@ -56,9 +56,7 @@
   systemd.services."systemd-importd".serviceConfig.WatchdogSec = 0;
   systemd.services."systemd-hostnamed".serviceConfig.WatchdogSec = 0;
   systemd.services."systemd-homed".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-networkd".serviceConfig.WatchdogSec =
-    lib.mkIf config.systemd.network.enable
-      0;
+  systemd.services."systemd-networkd".serviceConfig.WatchdogSec = lib.mkIf config.systemd.network.enable 0;
   # ssh config
   programs.ssh.extraConfig = ''
     Include /opt/orbstack-guest/etc/ssh_config
