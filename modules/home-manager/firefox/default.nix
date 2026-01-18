9c12bc15ba18a35a3acc10edb805c25e7a7f606b
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-01-19 01:02:38 +0300

    chore: remove non ASCII whitespace

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -5,11 +5,11 @@
     stylix.targets.firefox.profileNames = [ user.username ];
     # This is patch for Firefox to allow downgrading to profiles.ini.
     #
-    # Usefull links
-    # - https://github.com/bandithedoge/nixpkgs-firefox-nix-darwin/issues/14
-    # - https://github.com/nix-community/home-manager/issues/5717
-    # - https://github.com/nix-community/home-manager/pull/5801
-    # - https://github.com/nix-community/home-manager/pull/5723
+    # Usefull links
+    # - https://github.com/bandithedoge/nixpkgs-firefox-nix-darwin/issues/14
+    # - https://github.com/nix-community/home-manager/issues/5717
+    # - https://github.com/nix-community/home-manager/pull/5801
+    # - https://github.com/nix-community/home-manager/pull/5723
     home.sessionVariables = {
       MOZ_LEGACY_PROFILES = 1;
       MOZ_ALLOW_DOWNGRADE = 1;
