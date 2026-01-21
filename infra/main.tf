ac90e6416a1547ac93b806973f22867c11a5f27d
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-01-21 17:44:00 +0300

    feat: store Terraform state in R2 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = ">= 1.0"
+  backend "s3" {
+    key    = "nix-config/terraform.tfstate"
+    region = "auto"
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+  }
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
