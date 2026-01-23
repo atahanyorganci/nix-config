85446aaca1e3a6271c7a80409eb49ab7129106e8
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-01-23 14:26:35 +0300

    refactor: reimplement `homeModuels.gpg`

--- modules/home/gpg/default.nix
+++ modules/home/gpg/default.nix
@@ -1,28 +1,42 @@
 {
   pkgs,
   config,
+  lib,
   ...
-}: {
-  programs.gpg.enable = true;
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
-    includes = ["~/.orbstack/ssh/config"];
+}: let
+  isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
+  includes =
+    if isDarwin
+    then ["~/.orbstack/ssh/config"]
+    else [];
+in {
+  options.gpg.enable = lib. mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "Enable GPG";
   };
-  home.packages = with pkgs; [
-    openssl
-    yubikey-personalization
-    yubikey-manager
-  ];
-  services.gpg-agent = {
-    enable = true;
-    enableSshSupport = true;
-    enableBashIntegration = config.shell.bash.enable;
-    enableFishIntegration = config.shell.fish.enable;
-    enableNushellIntegration = config.shell.fish.enable;
-    enableZshIntegration = config.shell.zsh.enable;
-  };
-  home.sessionVariables = {
-    SSH_AUTH_SOCK = "${config.home.homeDirectory}/.gnupg/S.gpg-agent.ssh";
+  config = lib.mkIf config.gpg.enable {
+    programs.gpg.enable = true;
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      inherit includes;
+    };
+    home.packages = with pkgs; [
+      openssl
+      yubikey-personalization
+      yubikey-manager
+    ];
+    services.gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      enableBashIntegration = config.shell.bash.enable;
+      enableFishIntegration = config.shell.fish.enable;
+      enableNushellIntegration = config.shell.fish.enable;
+      enableZshIntegration = config.shell.zsh.enable;
+    };
+    home.sessionVariables = {
+      SSH_AUTH_SOCK = "${config.home.homeDirectory}/.gnupg/S.gpg-agent.ssh";
+    };
   };
 }
