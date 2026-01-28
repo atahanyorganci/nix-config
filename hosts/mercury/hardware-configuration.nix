a737397c38e44b9662f34d0fb882d8d45b90b014
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-01-28 20:58:04 +0300

    chore: update `mercury` host's hardware configuration

--- hosts/mercury/hardware-configuration.nix
+++ hosts/mercury/hardware-configuration.nix
@@ -7,27 +7,20 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "sdhci_pci"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sdhci_pci"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/3e0876cc-dcd9-467e-8f32-273e6ebe7bfa";
+    device = "/dev/disk/by-uuid/021f08b1-a877-4169-b6db-2a55833c1694";
     fsType = "ext4";
   };
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/39A0-F695";
+    device = "/dev/disk/by-uuid/6E88-B561";
     fsType = "vfat";
     options = ["fmask=0077" "dmask=0077"];
   };
   swapDevices = [];
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
