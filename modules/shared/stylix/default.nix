03dc9ee1ffccdf38dd380503a976262286dad43c
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-05-25 23:20:08 +0300

    refactor: change to Catpuccin Mocha theme

--- modules/shared/stylix/default.nix
+++ modules/shared/stylix/default.nix
@@ -4,7 +4,7 @@ in {
   fonts.packages = [monospace];
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     fonts = {
       monospace = {
         package = monospace;
