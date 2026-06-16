2061ca78ddd187d0664c49cbde500fa398e543c8
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-06-16 15:18:22 +0300

    feat: port Cursor Dark theme to Stylix

--- modules/shared/stylix/default.nix
+++ modules/shared/stylix/default.nix
@@ -4,7 +4,8 @@ in {
   fonts.packages = [monospace];
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    polarity = "dark";
+    base16Scheme = ./cursor-dark.yaml;
     fonts = {
       monospace = {
         package = monospace;
