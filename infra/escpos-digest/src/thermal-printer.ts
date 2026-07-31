cb2081eca18ddb165dc3551d0327b92a49c990a4
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-08-01 01:28:36 +0300

    feat(escpos-digest): decode PNG data URIs with Effect Schema
    Add pngjs-backed readImage so thermal Image src can be data:image/png;base64 without DOM APIs.

--- infra/escpos-digest/src/thermal-printer.ts
+++ infra/escpos-digest/src/thermal-printer.ts
@@ -1,5 +1,8 @@
 import * as Effect from "effect/Effect";
 import * as Schema from "effect/Schema";
+import * as SchemaParser from "effect/SchemaParser";
+import * as SchemaTransformation from "effect/SchemaTransformation";
+import { PNG } from "pngjs";
 import * as ReactThermalPrinter from "react-thermal-printer";
 
 type RenderParams = Parameters<typeof ReactThermalPrinter.render>;
@@ -46,3 +49,51 @@ export const makeTemplate = <A>(options: {
 	props: options.props,
 	render: (props: A, renderOptions?: RenderOptions) => render(options.render(props), renderOptions),
 });
+
+export class InvalidUrlError extends Schema.TaggedErrorClass<InvalidUrlError>()(
+	"InvalidUrlError",
+	{
+		url: Schema.String,
+	},
+	{ httpApiStatus: 400 },
+) {}
+
+export class PngDecodeError extends Schema.TaggedErrorClass<PngDecodeError>()(
+	"PngDecodeError",
+	{
+		cause: Schema.Unknown,
+	},
+	{ httpApiStatus: 500 },
+) {}
+
+export interface Image {
+	readonly data: Uint8Array;
+	readonly width: number;
+	readonly height: number;
+}
+
+const PngDataUri = Schema.TemplateLiteralParser([
+	"data:image/png;base64,",
+	Schema.String.check(Schema.isNonEmpty(), Schema.isBase64()).pipe(
+		Schema.decodeTo(Schema.Uint8Array, SchemaTransformation.uint8ArrayFromBase64String),
+	),
+]);
+
+export const readImage = Effect.fn(function* (src: string) {
+	const [, pngFile] = yield* SchemaParser.decodeUnknownEffect(PngDataUri)(src).pipe(
+		Effect.mapError(() => new InvalidUrlError({ url: src })),
+	);
+
+	const png = yield* Effect.try({
+		try: () => PNG.sync.read(Buffer.from(pngFile)),
+		catch: cause => new PngDecodeError({ cause }),
+	});
+
+	return {
+		data: new Uint8Array(png.data),
+		width: png.width,
+		height: png.height,
+	} satisfies Image;
+});
+
+export const readImageSync = (src: string) => Effect.runPromise(readImage(src));
