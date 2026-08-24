dd667448d9be3fd28ebb952e78fb11aa8b83ff69
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-08-24 12:40:43 +0300

    fix: update `eza --icons` to include `always`

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -10,9 +10,9 @@
     cfg = config.shell;
     shellAliases = {
       # `ll` - list files with long format with `eza`
-      ll = "${pkgs.eza}/bin/eza --long --header --icons";
+      ll = "${pkgs.eza}/bin/eza --long --header --icons always";
       # `tree` - list files in a tree format with `eza`
-      tree = "${pkgs.eza}/bin/eza --tree --long --header --icons";
+      tree = "${pkgs.eza}/bin/eza --tree --long --header --icons always";
       # `nd` - activate a development shell with default shell
       nd = "nix develop --command ${user.shell}";
     };
